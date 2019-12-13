94a9467fb6b176b6ddec3c533e5937469f72de12
flkt <flkt.crnpio@gmail.com>
2019-12-13 15:41:36 -0600

    working on

--- selection-sort.py
+++ selection-sort.py
@@ -1,9 +1,7 @@
 def selectionSort(arr):
-    arrSort = []
-    for i in range(len(arr)):
-        min = i
-        for j in range(i+1, len(arr))
-    return arrSort
+    return 'ahi voy'
+
+
 
 arr = [7,3,5,1,9,5,6]
 result = selectionSort(arr)
